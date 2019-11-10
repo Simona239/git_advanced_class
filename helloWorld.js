251f3a6dce5dbc15082617473b481843b1ce124b
Simona239 <simona.mikelionyte@gmail.com>
2019-11-10 15:11:37 +0100

    new function added

--- helloWorld.js
+++ helloWorld.js
@@ -2,6 +2,11 @@ const helloWorld = function() {
   return 'Hello world!';
 };
 
+const GoodbyeWorld = function() {
+  return 'Goodbye world!';
+};
+
 console.log(helloWorld());
+console.log(GoodbyeWorld());
 
 console.log("Almost there!");
